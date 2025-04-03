3fb25fa0715a6c66222d924b4a62522d69bf32a6
RajatDBazaar <dbrajat24@gmail.com>
2025-04-03 11:12:35 +0530

    commit

--- lib/features/contactus form/screen/contact_us_setion.dart
+++ lib/features/contactus form/screen/contact_us_setion.dart
@@ -51,168 +51,6 @@ class ContactUsSection extends StatelessWidget {
   }
 }
 
-class ContactUsInfoContainer extends StatelessWidget {
-  const ContactUsInfoContainer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final height = MediaQuery.sizeOf(context).height;
-    final width = MediaQuery.sizeOf(context).width;
-    final icon = Transform.rotate(
-      angle: 45 * 3.141592653589793 / 180,
-      child: Container(
-        width: 8,
-        height: 8,
-        color: AppColors.orange,
-        margin: const EdgeInsets.only(top: 2),
-      ),
-    );
-    return Container(
-      width: width * 0.34,
-      height: height * 0.8,
-      color: AppColors.black10,
-      padding: EdgeInsets.symmetric(
-        horizontal: width * 0.02,
-        vertical: height * 0.03,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: height * 0.011),
-          Row(
-            children: [
-              icon,
-              SizedBox(width: width * 0.01),
-              MyTextPoppines(
-                text: AppText.whatsNext,
-                fontSize: width * 0.011,
-                color: AppColors.white,
-                fontWeight: FontWeight.w600,
-              ),
-            ],
-          ),
-          SizedBox(height: height * 0.02),
-          MyTextPoppines(
-            text: AppText.onceYourEmailGetsToOurInbox,
-            fontSize: width * 0.0085,
-            color: AppColors.white,
-            fontWeight: FontWeight.w300,
-            height: 1.6,
-            maxLines: 10,
-          ),
-          SizedBox(height: height * 0.04),
-          Row(
-            children: [
-              icon,
-              SizedBox(width: width * 0.01),
-              MyTextPoppines(
-                text: AppText.theFirstPointOfContact,
-                fontSize: width * 0.011,
-                color: AppColors.white,
-                fontWeight: FontWeight.w600,
-              ),
-            ],
-          ),
-          SizedBox(height: height * 0.03),
-          // Photo Name Designation Line
-          buildProfile(
-            context: context,
-            image: "",
-            name: "Rajat Khoware",
-            post: "Founder",
-            quote: AppText.founderQuote,
-          ),
-          SizedBox(height: height * 0.02),
-          buildProfile(
-            context: context,
-            image: "",
-            name: "Akshay Sen",
-            post: "Co-Founder",
-            quote: AppText.coFounderQuote,
-          ),
-          SizedBox(height: height * 0.05),
-          Row(
-            children: [
-              icon,
-              SizedBox(width: width * 0.01),
-              MyTextPoppines(
-                text: AppText.moreContacts,
-                fontSize: width * 0.011,
-                color: AppColors.white,
-                fontWeight: FontWeight.w600,
-              ),
-            ],
-          ),
-          SizedBox(height: height * 0.03),
-          MyTextPoppines(
-            text: AppText.contactEmail,
-            fontSize: width * 0.008,
-            color: AppColors.white,
-            fontWeight: FontWeight.w400,
-          ),
-          SizedBox(height: height * 0.01),
-          MyTextPoppines(
-            text: AppText.bookACall,
-            fontSize: width * 0.008,
-            color: AppColors.orange,
-            fontWeight: FontWeight.w600,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget buildProfile({
-    required BuildContext context,
-    required String image,
-    required String name,
-    required String post,
-    required String quote,
-  }) {
-    final height = MediaQuery.sizeOf(context).height;
-    final width = MediaQuery.sizeOf(context).width;
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          width: width * 0.06,
-          height: height * 0.11,
-          color: AppColors.lightGreen,
-          // img
-        ),
-        SizedBox(width: width * 0.01),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            MyTextPoppines(
-              text: name,
-              fontSize: width * 0.01,
-              color: AppColors.orange,
-              fontWeight: FontWeight.w600,
-            ),
-            MyTextPoppines(
-              text: post,
-              fontSize: width * 0.008,
-              color: AppColors.white.withOpacity(0.6),
-              fontWeight: FontWeight.w400,
-            ),
-            SizedBox(height: height * 0.012),
-            SizedBox(
-              width: width * 0.2,
-              child: MyTextPoppines(
-                text: quote,
-                fontSize: width * 0.007,
-                color: AppColors.white,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-          ],
-        )
-      ],
-    );
-  }
-}
-
 class ContactUsBanner extends StatelessWidget {
   const ContactUsBanner({
     super.key,
@@ -405,6 +243,169 @@ class ContactUsContainer extends StatelessWidget {
     );
   }
 }
+
+// Important do not remove
+// class ContactUsInfoContainer extends StatelessWidget {
+//   const ContactUsInfoContainer({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final height = MediaQuery.sizeOf(context).height;
+//     final width = MediaQuery.sizeOf(context).width;
+//     final icon = Transform.rotate(
+//       angle: 45 * 3.141592653589793 / 180,
+//       child: Container(
+//         width: 8,
+//         height: 8,
+//         color: AppColors.orange,
+//         margin: const EdgeInsets.only(top: 2),
+//       ),
+//     );
+//     return Container(
+//       width: width * 0.34,
+//       height: height * 0.8,
+//       color: AppColors.black10,
+//       padding: EdgeInsets.symmetric(
+//         horizontal: width * 0.02,
+//         vertical: height * 0.03,
+//       ),
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           SizedBox(height: height * 0.011),
+//           Row(
+//             children: [
+//               icon,
+//               SizedBox(width: width * 0.01),
+//               MyTextPoppines(
+//                 text: AppText.whatsNext,
+//                 fontSize: width * 0.011,
+//                 color: AppColors.white,
+//                 fontWeight: FontWeight.w600,
+//               ),
+//             ],
+//           ),
+//           SizedBox(height: height * 0.02),
+//           MyTextPoppines(
+//             text: AppText.onceYourEmailGetsToOurInbox,
+//             fontSize: width * 0.0085,
+//             color: AppColors.white,
+//             fontWeight: FontWeight.w300,
+//             height: 1.6,
+//             maxLines: 10,
+//           ),
+//           SizedBox(height: height * 0.04),
+//           Row(
+//             children: [
+//               icon,
+//               SizedBox(width: width * 0.01),
+//               MyTextPoppines(
+//                 text: AppText.theFirstPointOfContact,
+//                 fontSize: width * 0.011,
+//                 color: AppColors.white,
+//                 fontWeight: FontWeight.w600,
+//               ),
+//             ],
+//           ),
+//           SizedBox(height: height * 0.03),
+//           // Photo Name Designation Line
+//           buildProfile(
+//             context: context,
+//             image: "",
+//             name: "Rajat Khoware",
+//             post: "Founder",
+//             quote: AppText.founderQuote,
+//           ),
+//           SizedBox(height: height * 0.02),
+//           buildProfile(
+//             context: context,
+//             image: "",
+//             name: "Akshay Sen",
+//             post: "Co-Founder",
+//             quote: AppText.coFounderQuote,
+//           ),
+//           SizedBox(height: height * 0.05),
+//           Row(
+//             children: [
+//               icon,
+//               SizedBox(width: width * 0.01),
+//               MyTextPoppines(
+//                 text: AppText.moreContacts,
+//                 fontSize: width * 0.011,
+//                 color: AppColors.white,
+//                 fontWeight: FontWeight.w600,
+//               ),
+//             ],
+//           ),
+//           SizedBox(height: height * 0.03),
+//           MyTextPoppines(
+//             text: AppText.contactEmail,
+//             fontSize: width * 0.008,
+//             color: AppColors.white,
+//             fontWeight: FontWeight.w400,
+//           ),
+//           SizedBox(height: height * 0.01),
+//           MyTextPoppines(
+//             text: AppText.bookACall,
+//             fontSize: width * 0.008,
+//             color: AppColors.orange,
+//             fontWeight: FontWeight.w600,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+
+//   Widget buildProfile({
+//     required BuildContext context,
+//     required String image,
+//     required String name,
+//     required String post,
+//     required String quote,
+//   }) {
+//     final height = MediaQuery.sizeOf(context).height;
+//     final width = MediaQuery.sizeOf(context).width;
+//     return Row(
+//       crossAxisAlignment: CrossAxisAlignment.start,
+//       children: [
+//         Container(
+//           width: width * 0.06,
+//           height: height * 0.11,
+//           color: AppColors.lightGreen,
+//           // img
+//         ),
+//         SizedBox(width: width * 0.01),
+//         Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             MyTextPoppines(
+//               text: name,
+//               fontSize: width * 0.01,
+//               color: AppColors.orange,
+//               fontWeight: FontWeight.w600,
+//             ),
+//             MyTextPoppines(
+//               text: post,
+//               fontSize: width * 0.008,
+//               color: AppColors.white.withOpacity(0.6),
+//               fontWeight: FontWeight.w400,
+//             ),
+//             SizedBox(height: height * 0.012),
+//             SizedBox(
+//               width: width * 0.2,
+//               child: MyTextPoppines(
+//                 text: quote,
+//                 fontSize: width * 0.007,
+//                 color: AppColors.white,
+//                 fontWeight: FontWeight.w400,
+//               ),
+//             ),
+//           ],
+//         )
+//       ],
+//     );
+//   }
+// }
 
 // class ContactUsContainer extends StatelessWidget {
 //   const ContactUsContainer({super.key});
